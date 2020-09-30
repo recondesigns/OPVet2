04e89422b3bfaa0124f0ec1d6f5187a25d60cbb3
Michael Stedman <michaelstedman81@gmail.com>
2020-09-30 01:03:17 -0600

    Added boilerplate
    Added boilderplate code, rendering App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+    border: 1px solid dodgerblue;
+`
+
+export default function App() {
+    return (
+        <AppContainer>
+            <h1>App Container</h1>
+        </AppContainer>
+    )
+}
