d51e51960dd994d21c54411e8c27fb523f79f89e
Michael Stedman <michaelstedman81@gmail.com>
2020-10-12 13:45:22 -0600

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 // import { AppStateContext } from './providers/Store'
 
 import { Header } from './components/Header'
-import { StatusMessage } from './components/StatusMessage'
 
 const AppContainer = styled.div`
     width: 100%;
@@ -16,7 +15,6 @@ export default function App() {
     return (
         <AppContainer>
             <Header />
-            <StatusMessage status={'active'} message={'Here is your new messages.'} />
         </AppContainer>
     )
 }
